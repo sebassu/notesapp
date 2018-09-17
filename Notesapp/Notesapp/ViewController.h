166e85d1ad0783e1686f70101595fdc4ea22b0b2
Sebastián Uriarte Güimil <suriarte@orangeloops.com>
2018-09-17 14:26:28 -0300

    Base project created.

---
+++ Notesapp/Notesapp/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Notesapp
+//
+//  Created by Sebastian Uriarte on 9/17/18.
+//  Copyright © 2018 OrangeLoops. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Notesapp/Notesapp/ViewController.m
@@ -0,0 +1,28 @@
+//
+//  ViewController.m
+//  Notesapp
+//
+//  Created by Sebastian Uriarte on 9/17/18.
+//  Copyright © 2018 OrangeLoops. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end

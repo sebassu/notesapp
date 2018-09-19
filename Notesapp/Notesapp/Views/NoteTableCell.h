5222f05a1207d4d74c879c4430728b0173fa8fd9
Sebastián Uriarte Güimil <suriarte@orangeloops.com>
2018-09-19 17:02:32 -0300

    Created NoteTableCell xib file and empty class definition.

---
+++ Notesapp/Notesapp/Views/NoteTableCell.h
@@ -0,0 +1,13 @@
+//
+//  NoteTableCell.h
+//  Notesapp
+//
+//  Created by Sebastián Uriarte Güimil on 9/19/18.
+//  Copyright © 2018 OrangeLoops. All rights reserved.
+//
+
+#ifndef NoteTableCell_h
+#define NoteTableCell_h
+
+
+#endif
---
+++ Notesapp/Notesapp/Views/NoteTableCell.m
@@ -0,0 +1,9 @@
+//
+//  NoteTableCell.m
+//  Notesapp
+//
+//  Created by Sebastián Uriarte Güimil on 9/19/18.
+//  Copyright © 2018 OrangeLoops. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
